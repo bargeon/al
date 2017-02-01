290b1988739ecb186918f04199c74ae7febcd92b
bargeon <barajaselian47@gmail.com>
2017-01-31 16:51:54 -0800

    fixed files names

---
+++ string_concatenation.py
@@ -0,0 +1,9 @@
+a_string = "Beautiful string"
+a_2_string = "Another beautiful string"
+
+print(a_string + ", " + a_2_string)
+print("Trying to concatenate a string('string') with an integer(42), or a floating point integer(42.0), will produce an error.")
+print("Example: print('string' + 42)")
+print("To work around this you can convert 42 into a string with str(42)")
+print("print('string' + str(42)")
+print("output: string42")
